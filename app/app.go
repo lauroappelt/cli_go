74e65def625b65ede62d4352693d1bc1bec9375e
lauro <lauro@solis.com.br>
2023-05-10 19:59:35 -0300

    Name server lookup

--- app/app.go
+++ app/app.go
@@ -13,17 +13,25 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação Linha de Comando"
 	app.Usage = "Busca Ips e Nomes de Servidores"
 
+	flags := []cli.Flag {
+		cli.StringFlag{
+			Name: "domain",
+			Value: "google.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command {
 		{
 			Name: "ip",
 			Usage: "Busca Ips de domínios na internet",
-			Flags: []cli.Flag {
-				cli.StringFlag{
-					Name: "domain",
-					Value: "google.com.br",
-				},
-			},
 			Action: BuscarIps,
+			Flags: flags,
+		},
+		{
+			Name: "server",
+			Usage: "Busca o nomes de servidores na internet",
+			Flags: flags,
+			Action: BuscarServidores,
 		},
 	}
 
@@ -31,14 +39,27 @@ func Gerar() *cli.App {
 }
 
 func BuscarIps(c *cli.Context) {
-	host := c.String("domain")
+	domain := c.String("domain")
 
-	ips, erro := net.LookupIP(host); 
+	ips, erro := net.LookupIP(domain); 
 	if erro != nil {
 		log.Fatal(erro)
 	}
 
 	for _ , ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func BuscarServidores (c *cli.Context) {
+	domain := c.String("domain")
+
+	servidores, erro := net.LookupNS(domain);
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _ , servidor := range servidores {
+		fmt.Println(servidor)
 	}
 }
